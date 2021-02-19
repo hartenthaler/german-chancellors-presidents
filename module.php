633a80d272aadede1a1e1978292f897a3f0fe839
Hermann Hartenthaler <hermann@hartenthaler.de>
2021-02-19 22:53:52 +0100

    made some strings translatable

--- module.php
+++ module.php
@@ -36,13 +36,13 @@ return new class extends AbstractModule implements ModuleCustomInterface, Module
     use ModuleCustomTrait;
     use ModuleHistoricEventsTrait;
 
-    public const CUSTOM_TITLE = 'German Chancellors Presidents';
+    public const CUSTOM_TITLE = 'German Chancellors Presidents 🇩🇪';
 
     public const CUSTOM_AUTHOR = 'Hermann Hartenthaler';
     
     public const CUSTOM_WEBSITE = 'https://github.com/hartenthaler/german-chancellors-presidents/';
     
-    public const CUSTOM_VERSION = '2.0.4.0';
+    public const CUSTOM_VERSION = '2.0.5.0';
 
     public const CUSTOM_LAST = 'https://github.com/hartenthaler/german-chancellors-presidents/blob/master/latest-version.txt';
 
@@ -173,42 +173,51 @@ return new class extends AbstractModule implements ModuleCustomInterface, Module
     /**
      * All events provided by this module.
      * 
-     * Each line is a GEDCOM style record to describe an event (EVEN), including newline chars (\n)
-     *      1 EVEN <title>
-     *      2 TYPE <short category name>
-     *      2 DATE <date or date period>
-     *      2 NOTE <comment or [wikipedia de](<link>)>
+     * Each line is a GEDCOM style record to describe an event (EVEN), including newline chars (\n); markdown is used for NOTE;
+     *      1 EVEN <name> (<party>)
+     *      2 TYPE <Chancellor|President> of Germany
+     *      2 DATE <date period>
+     *      2 NOTE [wikipedia de](<link>)
      *
-     * @return Collection<string>
      */
     
     public function historicEventsAll(): Collection
     {
+        $eventTypeC = I18N::translate('Chancellor of Germany');
+        $eventTypeP = I18N::translate('President of Germany');
+        $eventSubtypeA = I18N::translate('acting');
+        
+    /**
+     * tbd: wikipedia should be selected based on the language of the webtrees user if the following pages exist in his wikipedia language version
+     */
+        $wikipedia  = "de";
+        
         return new Collection([
-// Bundeskanzler:
-        "1 EVEN Konrad Adenauer (CDU)\n2 TYPE Bundeskanzler von Deutschland\n2 DATE FROM 15 SEP 1949 TO 16 OCT 1963\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Konrad_Adenauer)",
-        "1 EVEN Ludwig Erhard (CDU?)\n2 TYPE Bundeskanzler von Deutschland\n2 DATE FROM 16 OCT 1963 TO 1 DEC 1966\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Ludwig_Erhard)",
-        "1 EVEN Kurt Georg Kiesinger (CDU)\n2 TYPE Bundeskanzler von Deutschland\n2 DATE FROM 1 DEC 1966 TO 21 OCT 1969\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Kurt_Georg_Kiesinger)",
-        "1 EVEN Willy Brandt (SPD)\n2 TYPE Bundeskanzler von Deutschland\n2 DATE FROM 21 OCT 1969 TO 7 MAY 1974\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Willy_Brandt)",
-        "1 EVEN Walter Scheel (FDP)\n2 TYPE Bundeskanzler von Deutschland (nur geschäftsführend)\n2 DATE FROM 7 MAY 1974 TO 16 MAY 1974\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Walter_Scheel)",
-        "1 EVEN Helmut Schmidt (SPD)\n2 TYPE Bundeskanzler von Deutschland\n2 DATE FROM 16 MAY 1974 TO 1 OCT 1982\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Helmut_Schmidt)",
-        "1 EVEN Helmut Kohl (CDU)\n2 TYPE Bundeskanzler von Deutschland\n2 DATE FROM 1 OCT 1982 TO 27 OCT 1998\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Helmut_Kohl)",
-        "1 EVEN Gerhard Schröder (SPD)\n2 TYPE Bundeskanzler von Deutschland\n2 DATE FROM 27 OCT 1998 TO 22 NOV 2005\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Gerhard_Schröder)",
-        "1 EVEN Angela Merkel (CDU)\n2 TYPE Bundeskanzler von Deutschland\n2 DATE FROM 22 NOV 2005\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Angela_Merkel)",
-// Bundespräsidenten:            
-        "1 EVEN Theodor Heuss (FDP)\n2 TYPE Bundespräsident von Deutschland\n2 DATE FROM 12 SEP 1949 TO 12 SEP 1959\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Theodor_Heuss)",
-        "1 EVEN Heinrich Lübke (CDU)\n2 TYPE Bundespräsident von Deutschland\n2 DATE FROM 13 SEP 1959 TO 30 JUN 1969\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Heinrich_Lübke)",
-        "1 EVEN Gustav Heinemann (SPD)\n2 TYPE Bundespräsident von Deutschland\n2 DATE FROM 1 JUL 1969 TO 30 JUN 1974\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Gustav_Heinemann)",
-        "1 EVEN Walter Scheel (FDP)\n2 TYPE Bundespräsident von Deutschland\n2 DATE FROM 1 JUL 1974 TO 30 JUN 1979\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Walter_Scheel)",
-        "1 EVEN Karl Carstens (CDU)\n2 TYPE Bundespräsident von Deutschland\n2 DATE FROM 1 JUL 1979 TO 30 JUN 1984\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Karl_Carstens)",
-        "1 EVEN Richard von Weizsäcker (CDU)\n2 TYPE Bundespräsident von Deutschland\n2 DATE FROM 1 JUL 1984 TO 30 JUN 1994\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Richard_von_Weizsäcker)",
-        "1 EVEN Roman Herzog (CDU)\n2 TYPE Bundespräsident von Deutschland\n2 DATE FROM 1 JUL 1994 TO 30 JUN 1999\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Roman_Herzog)",
-        "1 EVEN Johannes Rau (SPD)\n2 TYPE Bundespräsident von Deutschland\n2 DATE FROM 1 JUL 1999 TO 30 JUN 2004\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Johannes_Rau)",
-        "1 EVEN Horst Köhler (CDU)\n2 TYPE Bundespräsident von Deutschland\n2 DATE FROM 1 JUL 2004 TO 31 MAY 2010\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Horst_Köhler)",
-        "1 EVEN Christian Wulff (CDU)\n2 TYPE Bundespräsident von Deutschland\n2 DATE FROM 30 JUN 2010 TO 17 FEB 2012\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Christian_Wulff)",
-        "1 EVEN Joachim Gauck (parteilos)\n2 TYPE Bundespräsident von Deutschland\n2 DATE FROM 18 MAR 2012 TO 18 MAR 2017\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Joachim_Gauck)",
-        "1 EVEN Frank-Walter Steinmeier (SPD)\n2 TYPE Bundespräsident von Deutschland\n2 DATE FROM 19 MAR 2017\n2 NOTE [wikipedia de](https://de.wikipedia.org/wiki/Frank-Walter_Steinmeier)",
+// Chancellors:
+        "1 EVEN Konrad Adenauer (CDU)\n2 TYPE ".$eventTypeC."\n2 DATE FROM 15 SEP 1949 TO 16 OCT 1963\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Konrad_Adenauer)",
+        "1 EVEN Ludwig Erhard (CDU?)\n2 TYPE ".$eventTypeC."\n2 DATE FROM 16 OCT 1963 TO 1 DEC 1966\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Ludwig_Erhard)",
+        "1 EVEN Kurt Georg Kiesinger (CDU)\n2 TYPE ".$eventTypeC."\n2 DATE FROM 1 DEC 1966 TO 21 OCT 1969\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Kurt_Georg_Kiesinger)",
+        "1 EVEN Willy Brandt (SPD)\n2 TYPE ".$eventTypeC."\n2 DATE FROM 21 OCT 1969 TO 7 MAY 1974\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Willy_Brandt)",
+        "1 EVEN Walter Scheel (FDP)\n2 TYPE ".$eventTypeC." (".$eventSubtypeA.")\n2 DATE FROM 7 MAY 1974 TO 16 MAY 1974\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Walter_Scheel)",
+        "1 EVEN Helmut Schmidt (SPD)\n2 TYPE ".$eventTypeC."\n2 DATE FROM 16 MAY 1974 TO 1 OCT 1982\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Helmut_Schmidt)",
+        "1 EVEN Helmut Kohl (CDU)\n2 TYPE ".$eventTypeC."\n2 DATE FROM 1 OCT 1982 TO 27 OCT 1998\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Helmut_Kohl)",
+        "1 EVEN Gerhard Schröder (SPD)\n2 TYPE ".$eventTypeC."\n2 DATE FROM 27 OCT 1998 TO 22 NOV 2005\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Gerhard_Schröder)",
+        "1 EVEN Angela Merkel (CDU)\n2 TYPE ".$eventTypeC."\n2 DATE FROM 22 NOV 2005\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Angela_Merkel)",
+// Presidents (without acting presidents):            
+        "1 EVEN Theodor Heuss (FDP)\n2 TYPE ".$eventTypeP."\n2 DATE FROM 12 SEP 1949 TO 12 SEP 1959\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Theodor_Heuss)",
+        "1 EVEN Heinrich Lübke (CDU)\n2 TYPE ".$eventTypeP."\n2 DATE FROM 13 SEP 1959 TO 30 JUN 1969\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Heinrich_Lübke)",
+        "1 EVEN Gustav Heinemann (SPD)\n2 TYPE ".$eventTypeP."\n2 DATE FROM 1 JUL 1969 TO 30 JUN 1974\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Gustav_Heinemann)",
+        "1 EVEN Walter Scheel (FDP)\n2 TYPE ".$eventTypeP."\n2 DATE FROM 1 JUL 1974 TO 30 JUN 1979\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Walter_Scheel)",
+        "1 EVEN Karl Carstens (CDU)\n2 TYPE ".$eventTypeP."\n2 DATE FROM 1 JUL 1979 TO 30 JUN 1984\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Karl_Carstens)",
+        "1 EVEN Richard von Weizsäcker (CDU)\n2 TYPE ".$eventTypeP."\n2 DATE FROM 1 JUL 1984 TO 30 JUN 1994\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Richard_von_Weizsäcker)",
+        "1 EVEN Roman Herzog (CDU)\n2 TYPE ".$eventTypeP."\n2 DATE FROM 1 JUL 1994 TO 30 JUN 1999\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Roman_Herzog)",
+        "1 EVEN Johannes Rau (SPD)\n2 TYPE ".$eventTypeP."\n2 DATE FROM 1 JUL 1999 TO 30 JUN 2004\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Johannes_Rau)",
+        "1 EVEN Horst Köhler (CDU)\n2 TYPE ".$eventTypeP."\n2 DATE FROM 1 JUL 2004 TO 31 MAY 2010\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Horst_Köhler)",
+        "1 EVEN Christian Wulff (CDU)\n2 TYPE ".$eventTypeP."\n2 DATE FROM 30 JUN 2010 TO 17 FEB 2012\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Christian_Wulff)",
+        "1 EVEN Joachim Gauck (parteilos)\n2 TYPE ".$eventTypeP."\n2 DATE FROM 18 MAR 2012 TO 18 MAR 2017\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Joachim_Gauck)",
+        "1 EVEN Frank-Walter Steinmeier (SPD)\n2 TYPE ".$eventTypeP."\n2 DATE FROM 19 MAR 2017\n2 NOTE [wikipedia ".$wikipedia."](https://".$wikipedia.".wikipedia.org/wiki/Frank-Walter_Steinmeier)",
         ]);
     }
     
 };
+ 
